8a5ea5c8c305843f70e242f2de6e4e57a2c74eba
Andrés Peña Diaz <ca.penad@uniandes.edu.co>
2022-10-05 01:16:56 -0500

    Logs and comments implemented for both scripts

--- Lab 3/clientes.py
+++ Lab 3/clientes.py
@@ -1,58 +1,78 @@
-# import socket programming library
 import hashlib
-import os
 import socket
-import time
-# import thread module
-from _thread import *
-from multiprocessing.connection import wait
 from threading import Event, Thread
+import datetime
+import os
 
 port = 12345
-BUFFER_SIZE = 2048 # send 1024 bytes each time step
+BUFFER_SIZE = 2048  # Send 2048 bytes each time step
+# Create the log file name
+name_log = str(datetime.datetime.now()).replace(' ', '').replace(':', '-').split('.')[0]
+name_log += '-log.txt'
+# Create the log file
+log_file = open('./Logs/' + name_log, 'w')
 
-
-
-# thread function
-def clientOperation(event,socket,id,numClientes):
+# Thread function
+def clientOperation(socket, id, numClientes):
+    received_hash = ''
+    # Instantiate the MD5 algorithm for hashing the files
     md5 = hashlib.md5()
-    
-    file = open("/Users/Gabriel/Documents/Semestre 7/Infracom/Labs/Lab 3/ArchivosRecibidos/Cliente"+str(id)+"-Prueba"+str(numClientes), "wb")
-    socket.send(str(id).encode())
-    print("Cliente", id, "listo" )
+    filename ="Cliente" + str(id) + "-Prueba" + str(numClientes) + ".txt"
+    filepath = "./ArchivosRecibidos/Cliente" + str(id) + "-Prueba" + str(numClientes) + '.txt'
+    # Create the file that is going to be transferred
+    file = open(filepath, "wb")
+    # Send confirmation to server that is ready to receive the file
+    socket.send(str(socket.getsockname()[1]).encode())
+    print('Data transfer initiated for client:', socket.getsockname(),'\n-------------------------------------')
+    start_time = datetime.datetime.now().timestamp()
+    end_time = datetime.datetime.now().timestamp()
     while True:
-            # read 1024 bytes from the socket (receive)
-            bytes_read = socket.recv(BUFFER_SIZE)
-            if '<SEP>'.encode() in bytes_read:
-                received_hash = bytes_read.decode().split('<SEP>')[1]
-                print("Received hash", received_hash) 
-                
-                break
-            if not bytes_read:
-                continue
-            # write to the file the bytes we just received
-            md5.update(bytes_read)
-            file.write(bytes_read)
+        # Read 2048 bytes from the socket (receive)
+        bytes_read = socket.recv(BUFFER_SIZE)
+        # Asks if the separator is in the bytes read
+        if '<SEP>'.encode() in bytes_read:
+            # Separates the hashcode from the bytes read
+            end_time = datetime.datetime.now().timestamp()
+            received_hash = bytes_read.decode().split('<SEP>')[1]
+            break
+        # Waits for the server to send the bytes
+        if not bytes_read:
+            continue
+        # Update the MD5 with bytes read to complete hash
+        md5.update(bytes_read)
+        # Write to the file the bytes we just received
+        file.write(bytes_read)
     file.close()
-    print("[CLIENT {0}]:Received file's calculated hash: {1}".format(id,md5.hexdigest()))
-    #s.close()
+    print("[CLIENT {0}]: Received file's calculated hash: {1}".format(socket.getsockname(), md5.hexdigest()))
+    print('[CLIENT {0}]: Hash from server: {1}'.format(socket.getsockname(), received_hash))
+    print('[CLIENT {0}]: Integrity of data is:'.format(socket.getsockname()), received_hash == md5.hexdigest(),'\n-------------------------------------')
+    filesize = os.path.getsize(filepath)
+    log_file.write('The file is ({0}) selected for the test and it is: {1}\n'.format(filename, filesize/1000000))
+    log_file.write('Connected client: ' + str(socket.getsockname()) + '\n')
+    log_file.write('Client ' + str(socket.getsockname()) + ' transmission has successfully ended.' + '\n')
+    log_file.write('The file received on Client ' + str(socket.getsockname()) + 'took: ' + str((end_time - start_time)) + ' ms.' + '\n')
+    socket.close()
 
-def MainClientThread(cv, numClientes):
-    # local host IP '127.0.0.1'
+
+def MainClientThread(numClientes):
     host = 'localhost'
- 
-    # Define the port on which you want to connect
-    for x in range(1,numClientes +1):
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
- 
-        # connect to server on local computer
-        s.connect((host,port))
-        cliente = Thread(name=x , target= clientOperation, args=(cv,s,x,numClientes))
+    # Instantiate N Threads for the number of clients selected
+    for x in range(1, numClientes + 1):
+        # Instantiate a socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect socket to the server on localhost
+        s.connect((host, port))
+        # Create the client
+        cliente = Thread(name = x, target = clientOperation, args = (s, x, numClientes))
+        # Starts the client process
         cliente.start()
-        #cliente.join()
-    #s.close()
+
+
 if __name__ == '__main__':
-    cv = Event()
+    # Asks for the number of clients required for the transfer
     numClientes = int(input("Ingrese el numero de clientes: (max 25)\n"))
-    client = Thread(target=MainClientThread, args=(cv,numClientes))
+    os.system('clear')
+    # Instantiate a Superior Thread
+    client = Thread(target=MainClientThread, args = (numClientes,))
+    # Starts the thread to MainClientThread function
     client.start()
--- Lab 3/server.py
+++ Lab 3/server.py
@@ -1,99 +1,104 @@
-# import socket programming library
 import hashlib
-import os
 import socket
-import time
-# import thread module
-from _thread import *
-from multiprocessing.connection import wait
+import threading
 from threading import Event, Thread
-import select
-
+import datetime
+import os
 
 port = 12345
-BUFFER_SIZE = 2048 # send 1024 bytes each time step
-clientesConectados = 0
+BUFFER_SIZE = 2048  # Send 2048 bytes each time step
+clientesListos = 0
+# Create the log file name
+name_log = str(datetime.datetime.now()).replace(' ', '').replace(':', '-').split('.')[0]
+name_log += '-log.txt'
+# Create the log file
+log_file = open('./Logs/' + name_log, 'w')
+print_lock = threading.Lock()
 
-def serverOperation(connection, event, filename):
+def serverOperation(connection, event, filename, addr):
+    # Opening file reading binary
     file = open(filename, "rb")
+    # Instantiate the MD5 algorithm for hashing the files
     md5 = hashlib.md5()
-
+    # Receive confirmation of client for transfer initiation with the id of client
     confirmation = connection.recv(2048)
-    print( "Cliente ",confirmation.decode(), " listo")
-    global clientesConectados
-    clientesConectados +=1
+    # Deserialize client id
+    print("Client", addr, "sent confirmation")
+    # Adds 1 to check the number of clients connected
+    global clientesListos
+    clientesListos += 1
+    # Wait for all the clients to be connected
     event.wait()
-    print("Inicia envio")
-    
-    #c.send(calculated_hash.encode())
+    package_n = 1
+    start_time = datetime.datetime.now().timestamp()
+    end_time = datetime.datetime.now().timestamp()
     while True:
-        # read the bytes from the file
+        # Read the bytes from the file
         bytes_read = file.read(BUFFER_SIZE)
+        # If bytes_read is null sending is done
         if not bytes_read:
-            print("Done Sending")
+            end_time = datetime.datetime.now().timestamp()
             break
-        # we use sendall to assure transimission in 
-        # busy networks
+        # Updates MD5 with the bytes_read
         md5.update(bytes_read)
-        connection.send(bytes_read) 
-    calculated_hash = md5.hexdigest()
-    print("File's hash", calculated_hash)
-    connection.send('<SEP>'.encode())
-    connection.send(calculated_hash.encode())
-    connection.close()
+        # Sends the byte package to client
+        connection.send(bytes_read)
+        package_n += 1
+    with print_lock:
+        print('Client ' + str(addr) + ': received ' + str(package_n) + ' packages.')
+        # Calculates the hash of the file sent
+        calculated_hash = md5.hexdigest()
+        # Send the hash with a separator
+        connection.send('<SEP>'.encode())
+        connection.send(calculated_hash.encode())
+        connection.close()
+        print('Client' + str(addr) + ': Hash of the file sent: ' + str(calculated_hash) +'\n-------------------------------------')
+    package_n += 1
+    log_file.write('Client ' + str(addr) + ' transmission has successfully ended.'+'\n')
+    log_file.write('The file sent to Client ' + str(addr) + 'took: ' + str((end_time - start_time)) + ' ms.'+'\n')
 
 
-def MainServerThread(event,numArchivo,numClientes):
-    # the name of file we want to send, make sure it exists
-    # get the file size
+def MainServerThread(event, numArchivo, numClientes):
+
+    # The type of file we want to send
     if numArchivo == 1:
         filename = "150"
     else:
         filename = "250"
-    #filesize = os.path.getsize(filename)    
-    
-    host = ""
-
- 
-    # reserve a port on your computer
-    # in our case it is 12345 but it
-    # can be anything
+    filesize = os.path.getsize(filename)
+    log_file.write('The file is ({0}) selected for the test and it is: {1}\n'.format(filename, filesize/1000000))
+    print('The file selected for the test is: ' + filename)
+    host = "localhost"
+    # Instantiate a socketΩ
     socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Autoconnect to the socket on the port
     socketClient.bind((host, port))
-    print("Servidor corriendo en puerto: ", port)
- 
-    # put the socket into listening mode
+    print('Server is running on the port: ' + str(port))
+
+    # Put the socket into listening mode
     socketClient.listen(5)
-    read_list = [socketClient]
-    print("Socket escuchando")
- 
-    global clientesConectados
-    while True:
-        #print("Esperando clientes, conectados:",clientesConectados)
-        if clientesConectados < numClientes:
-            readable, writable, errored = select.select(read_list, [], [])
-            time.sleep(0.5)
-            for s in readable:
-                if s is socketClient:
-                    # establish connection with client
-                    connection, addr = socketClient.accept()    
-                    # Start a new thread and return its identifier
-                    read_list.append(connection)
-                    serverHandler = Thread(target=serverOperation, args=(connection, event, filename))
-                    serverHandler.start()
-            #clientesConectados es modificado dentro de cada thread para esperar la confirmacion del cliente
-            #clientesConectados += 1
-            #print(clientesConectados, " Clientes conectados")
-        else:
-            print(clientesConectados, " Clientes conectados")
-            event.set()
-            break
-    time.sleep(10)
+    print('Server is waiting for client connections\n-------------------------------------')
+    clientesConectados = 0
+    while clientesConectados < numClientes:
+        connection, addr = socketClient.accept()
+        clientesConectados += 1
+        # Start a new thread and return its identifier
+        serverHandler = Thread(target=serverOperation, args=(connection, event, filename, addr))
+        log_file.write('Connected client: ' + str(addr)+'\n')
+        serverHandler.start()
+        print('Client connected to server, data transfer starting')
+    while(clientesListos < numClientes):
+        continue
+    print('All clients are connected\n-------------------------------------')
+    event.set()
     socketClient.close()
+
 
 if __name__ == '__main__':
     cv = Event()
-    numClientes = int(input("Ingrese el numero de clientes: (max 25)\n"))
+    numClientes = int(input("Ingrese el numero de clientes: (Max 25)\n"))
+    os.system('clear')
     numArchivo = int(input("Que archivo desea usar:\n 1. 150 MB \n 2. 250 MB\n"))
-    server = Thread(target=MainServerThread, args=(cv,numArchivo,numClientes))
+    os.system('clear')
+    server = Thread(target=MainServerThread, args=(cv, numArchivo, numClientes))
     server.start()
